19658474cd765aa3ca732be578f6124642965377
Praddy2009 <Praddy2009@gmail.com>
2021-02-24 21:45:02 +0530

    final

---
+++ ARCore/README.md
@@ -0,0 +1,15 @@
+ARcore: https://github.com/google-ar
+
+ARFoundation 3.1.3
+ARCoreXR plugin 3.1.3
+
+```
+1. Intro to Solution Challange 2021
+2. Whats the scope of AR
+3. Getting started with Unity
+4. Intro to ARCore ARKit and lastly ARFoundation
+5. Unity Clock making.
+6. ARFoundation
+7. Final Resource sharing
+
+```
---
+++ C#/Scripts/1.Clock_Script/timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class timer : MonoBehaviour
+{
+
+    string hour,min,sec;
+    public TextMeshProUGUI text;
+    // Update is called once per frame
+    void Update()
+    {
+        timetell();
+    }
+
+    public void timetell(){
+        DateTime tim= DateTime.Now;
+        hour=tim.Hour.ToString();
+        min=tim.Minute.ToString();
+        sec=tim.Second.ToString();
+        text.text=hour+":"+min+":"+sec; 
+    }
+}
